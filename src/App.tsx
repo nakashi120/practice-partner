dded9a04418221bcc0873d1836a72a1072bf6991
nakashi120 <nakashi120@gmail.com>
2021-03-14 19:02:08 +0900

    clean up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-
-import WantedPage from "./components/pages/WantedPage"
-import HomePage from "./components/pages/HomePage"
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/wanteds" component={WantedPage} exact />
-        <Route path="/" component={HomePage} exact />
-      </Switch>
-    </Router>
-  )
+  return <div>App.tsx</div>
 }
 
 export default App
