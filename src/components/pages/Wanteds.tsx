973d5cdf4b794819c84b1bcbfd95fdc7c453768a
nakashi120 <nakashi120@gmail.com>
2021-03-17 23:08:59 +0900

    create mapping function for table contents

--- src/components/pages/Wanteds.tsx
+++ src/components/pages/Wanteds.tsx
@@ -14,61 +14,76 @@ import {
   Tr,
 } from "@chakra-ui/react"
 import { memo, useCallback, VFC } from "react"
-import { Link, useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
+
+const sampleData = [
+  {
+    id: 1,
+    title: "美容師の練習",
+    content: "髪の長い人の髪を切る練習",
+    price: 1000,
+    totalNum: 10,
+    applicationNum: 3,
+  },
+  {
+    id: 2,
+    title: "ネイルの練習",
+    content: "爪の長い人のネイルを整える練習",
+    price: 1000,
+    totalNum: 10,
+    applicationNum: 7,
+  },
+]
 
 export const Wanteds: VFC = memo(() => {
   const history = useHistory()
 
-  const onClickDetail = useCallback(() => history.push("/wanteds/100"), [
-    history,
-  ])
+  const onClickDetail = useCallback(
+    (id: number) => history.push(`/wanteds/${id}`),
+    [history]
+  )
 
   return (
     <Flex align="center" justify="center" margin={8}>
       <Box bg="white" w="4xl" p={4} borderRadius="md" shadow="md">
-        <Heading fontSize="lg">募集中案件</Heading>
+        <Heading fontSize="lg" my={4}>
+          WANTEDS LIST
+        </Heading>
         <Table fontSize="sm">
           <Thead>
             <Tr>
               <Th>ID</Th>
               <Th>TITLE</Th>
               <Th>CONTENT</Th>
-              <Th>料金</Th>
+              <Th>PRICE</Th>
               <Th>応募状況</Th>
               <Th></Th>
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>1</Td>
-              <Td>美容師の練習</Td>
-              <Td>髪の長い女性の人の練習</Td>
-              <Td>¥1,000</Td>
-              <Td>
-                <CircularProgress value={40} color="green.400">
-                  <CircularProgressLabel>40%</CircularProgressLabel>
-                </CircularProgress>
-              </Td>
-              <Td>
-                <Button size="sm" onClick={onClickDetail}>
-                  詳細
-                </Button>
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>2</Td>
-              <Td>ネイルの練習</Td>
-              <Td>爪が長い人の練習</Td>
-              <Td>¥1,000</Td>
-              <Td>
-                <Progress value={40} />
-              </Td>
-              <Td>
-                <Button size="sm" onClick={onClickDetail}>
-                  詳細
-                </Button>
-              </Td>
-            </Tr>
+            {sampleData.map((data) => (
+              <Tr>
+                <Td>{data.id}</Td>
+                <Td>{data.title}</Td>
+                <Td>{data.content}</Td>
+                <Td>{data.price}</Td>
+                <Td>
+                  <CircularProgress
+                    value={(data.applicationNum / data.totalNum) * 100}
+                    color="green.400"
+                  >
+                    <CircularProgressLabel>{`${
+                      (data.applicationNum / data.totalNum) * 100
+                    }%`}</CircularProgressLabel>
+                  </CircularProgress>
+                </Td>
+                <Td>
+                  <Button size="sm" onClick={() => onClickDetail(data.id)}>
+                    詳細
+                  </Button>
+                </Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </Box>
