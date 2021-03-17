a5ce68dab205780d63bd21fa30569983d5f63acf
nakashi120 <nakashi120@gmail.com>
2021-03-17 21:51:21 +0900

    create sample table for wanteds

--- src/components/pages/Wanteds.tsx
+++ src/components/pages/Wanteds.tsx
@@ -1,12 +1,77 @@
-// import { Link } from "@chakra-ui/react"
-import { memo, VFC } from "react"
-import { Link } from "react-router-dom"
+import {
+  Box,
+  Button,
+  CircularProgress,
+  CircularProgressLabel,
+  Flex,
+  Heading,
+  Progress,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react"
+import { memo, useCallback, VFC } from "react"
+import { Link, useHistory } from "react-router-dom"
 
 export const Wanteds: VFC = memo(() => {
+  const history = useHistory()
+
+  const onClickDetail = useCallback(() => history.push("/wanteds/100"), [
+    history,
+  ])
+
   return (
-    <div>
-      <p>募集中案件ページです。</p>
-      <Link to="/wanteds/100">詳細</Link>
-    </div>
+    <Flex align="center" justify="center" margin={8}>
+      <Box bg="white" w="4xl" p={4} borderRadius="md" shadow="md">
+        <Heading fontSize="lg">募集中案件</Heading>
+        <Table fontSize="sm">
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>TITLE</Th>
+              <Th>CONTENT</Th>
+              <Th>料金</Th>
+              <Th>応募状況</Th>
+              <Th></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>1</Td>
+              <Td>美容師の練習</Td>
+              <Td>髪の長い女性の人の練習</Td>
+              <Td>¥1,000</Td>
+              <Td>
+                <CircularProgress value={40} color="green.400">
+                  <CircularProgressLabel>40%</CircularProgressLabel>
+                </CircularProgress>
+              </Td>
+              <Td>
+                <Button size="sm" onClick={onClickDetail}>
+                  詳細
+                </Button>
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>2</Td>
+              <Td>ネイルの練習</Td>
+              <Td>爪が長い人の練習</Td>
+              <Td>¥1,000</Td>
+              <Td>
+                <Progress value={40} />
+              </Td>
+              <Td>
+                <Button size="sm" onClick={onClickDetail}>
+                  詳細
+                </Button>
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Box>
+    </Flex>
   )
 })
